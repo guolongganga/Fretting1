868eccf6a7cb02fe45c9c9c81f41bbca5004ffd8
曾苏娃 <zengsuwa@touzila.com>
2017-12-14 11:20:08 +0800

    银行卡号，变更银行卡号，呼叫客服

---
+++ app/src/main/java/com/zhsoft/fretting/ui/activity/user/BankCardActivity.java
@@ -0,0 +1,67 @@
+package com.zhsoft.fretting.ui.activity.user;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.zhsoft.fretting.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.droidlover.xdroidmvp.mvp.XActivity;
+
+/**
+ * 作者：sunnyzeng on 2017/12/14 10:07
+ * 描述：
+ */
+
+public class BankCardActivity extends XActivity {
+    @BindView(R.id.head_back) ImageButton headBack;
+    @BindView(R.id.head_title) TextView headTitle;
+    @BindView(R.id.image_banck) ImageView imageBanck;
+    @BindView(R.id.banck_name) TextView banckName;
+    @BindView(R.id.trade_number) TextView tradeNumber;
+    @BindView(R.id.btn_change) Button btnChange;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_user_bankcard;
+    }
+
+    @Override
+    public Object newP() {
+        return null;
+    }
+
+    @Override
+    public void initData(Bundle bundle) {
+        initView();
+    }
+
+    private void initView() {
+        headBack.setVisibility(View.VISIBLE);
+        headTitle.setText("我的银行卡");
+    }
+
+    @Override
+    public void initEvents() {
+        headBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(BankCardChangeActivity.class);
+            }
+        });
+
+    }
+
+
+}
