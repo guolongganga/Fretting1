ad727e2f7a86d4351f85ff106ed2b74d55f8b006
曾苏娃 <zengsuwa@touzila.com>
2018-01-08 18:06:09 +0800

    基金购买页面

---
+++ app/src/main/java/com/zhsoft/fretting/ui/activity/fund/BuySuccessActivity.java
@@ -0,0 +1,32 @@
+package com.zhsoft.fretting.ui.activity.fund;
+
+import android.os.Bundle;
+
+import cn.droidlover.xdroidmvp.mvp.XActivity;
+
+/**
+ * 作者：sunnyzeng on 2018/1/8 17:59
+ * 描述：购买成功页
+ */
+
+public class BuySuccessActivity extends XActivity{
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public Object newP() {
+        return null;
+    }
+
+    @Override
+    public void initData(Bundle bundle) {
+
+    }
+
+    @Override
+    public void initEvents() {
+
+    }
+}
