a87fc7ae55f3652f0fd7730090a8df9efda3a370
guolonggang0914 <15501261520@163.com>
2019-06-06 17:11:44 +0800

    提交清单文件,增加短信验证码类

---
+++ app/src/main/java/cn/com/buyforyou/fund/model/fund/CheckUserPictureResp.java
@@ -0,0 +1,11 @@
+package cn.com.buyforyou.fund.model.fund;
+
+import cn.com.buyforyou.fund.model.BaseResp;
+
+/**
+ * Created by guolonggang on 2019/4/28.
+ * Description:
+ */
+
+public class CheckUserPictureResp extends BaseResp {
+}
