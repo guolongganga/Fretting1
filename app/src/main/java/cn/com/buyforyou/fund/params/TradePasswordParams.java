8ec0e1fe2ae3bf012b6d7f49a793967fa18bcb3a
guolonggang0914 <15501261520@163.com>
2019-06-10 15:12:38 +0800

    提交清单文件,增加短信验证码类

---
+++ app/src/main/java/cn/com/buyforyou/fund/params/TradePasswordParams.java
@@ -0,0 +1,18 @@
+package cn.com.buyforyou.fund.params;
+
+/**
+ * Created by guolonggang on 2018/12/12.
+ * Description:  交易密码
+ */
+
+public class TradePasswordParams   {
+    private String  tradePpassword;
+
+    public String getTradePpassword() {
+        return tradePpassword;
+    }
+
+    public void setTradePpassword(String tradePpassword) {
+        this.tradePpassword = tradePpassword;
+    }
+}
