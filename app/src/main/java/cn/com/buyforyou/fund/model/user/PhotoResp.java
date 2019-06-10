a81f3d7a3019dce6d555b66305551cea0375597d
guolonggang0914 <15501261520@163.com>
2019-06-10 10:02:41 +0800

    提交清单文件,增加短信验证码类

---
+++ app/src/main/java/cn/com/buyforyou/fund/model/user/PhotoResp.java
@@ -0,0 +1,36 @@
+package cn.com.buyforyou.fund.model.user;
+
+import cn.com.buyforyou.fund.model.BaseResp;
+
+/**
+ * Created by guolonggang on 2019/4/17.
+ * Description:上传图片
+ */
+
+public class PhotoResp extends BaseResp<PhotoResp> {
+//
+//    private String message;
+//    private int status;
+//
+//
+//
+//    @Override
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    @Override
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+//
+//    @Override
+//    public int getStatus() {
+//        return status;
+//    }
+//
+//    @Override
+//    public void setStatus(int status) {
+//        this.status = status;
+//    }
+}
