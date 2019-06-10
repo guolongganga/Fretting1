f320d4dbbf21579c1643f6f8735bd48a6a2a3436
guolonggang0914 <15501261520@163.com>
2019-06-10 15:20:02 +0800

    提交清单文件,增加短信验证码类

--- app/src/main/java/cn/com/buyforyou/fund/present/user/SettingPresent.java
+++ app/src/main/java/cn/com/buyforyou/fund/present/user/SettingPresent.java
@@ -1,9 +1,13 @@
 package cn.com.buyforyou.fund.present.user;
 
+import android.util.Log;
+
 import cn.com.buyforyou.fund.R;
 import cn.com.buyforyou.fund.constant.Constant;
-import cn.com.buyforyou.fund.model.BaseResp;
+
 import cn.com.buyforyou.fund.model.user.RiskInfoResp;
+import cn.com.buyforyou.fund.model.user.UserInforResp;
+import cn.com.buyforyou.fund.model.user.UserInformationResp;
 import cn.com.buyforyou.fund.net.Api;
 import cn.com.buyforyou.fund.params.CommonReqData;
 import cn.com.buyforyou.fund.ui.activity.user.SettingActivity;
@@ -20,6 +24,43 @@ import cn.droidlover.xdroidmvp.net.XApi;
  */
 
 public class SettingPresent extends XPresent<SettingActivity> {
+    /**
+     * 用户的个人信息
+     *
+     */
+    public void userMessageInformation(String token, String userId) {
+        CommonReqData reqData = new CommonReqData();
+        reqData.setUserId(userId);
+        reqData.setToken(token);
+
+        Api.getApi()
+                .userMessage(reqData)
+                .compose(XApi.<UserInformationResp>getApiTransformer())
+                .compose(XApi.<UserInformationResp>getScheduler())
+                .compose(getV().<UserInformationResp>bindToLifecycle())
+                .subscribe(new ApiSubscriber<UserInformationResp>() {
+                    @Override
+                    public void onNext(UserInformationResp informationResp) {
+                        if (informationResp != null && informationResp.getStatus() == 200) {
+                            getV().userMessageData(informationResp);
+                        } else if (informationResp != null && informationResp.getStatus() == Constant.NO_LOGIN_STATUS) {
+                            getV().showToast(informationResp.getMessage());
+                            getV().areadyLogout();
+                        } else {
+                            getV().UserMessageFail();
+                            getV().showToast(informationResp.getMessage());
+                            XLog.e("返回数据为空");
+                        }
+                    }@Override
+                    protected void onFail(NetError error) {
+                        getV().UserMessageFail();
+                       // Log.e("Setting", "onFail: "+error.getMessage() );
+                        getV().showToast(R.string.request_error);
+                    }
+                });
+
+    }
+
 
     /**
      * 风险等级或是否做了风险测评 服务电话
